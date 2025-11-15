252a9e4dbc56c359220aae9f6b7926b8fc18b91b
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-11-15 23:44:27 +0100

    Setup npm publish

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,17 +1,10 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/rollup-plugin";
-import { extname } from "node:path";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
   platform: "neutral",
   plugins: [
-    tsSelfTypesPlugin(),
-    vanillaExtractPlugin({
-      extract: {
-        name: "styles.css",
-        sourcemap: false,
-      },
-    }),
+    vanillaExtractPlugin({ extract: { name: "styles.css", sourcemap: false } }),
   ],
   exports: {
     customExports(pkg) {
@@ -30,34 +23,3 @@ export default defineConfig({
     },
   },
 });
-
-function tsSelfTypesPlugin(): any {
-  return {
-    name: "ts-self-types",
-
-    async generateBundle(_options: any, bundle: any) {
-      for (const [_fileName, asset] of Object.entries(bundle)) {
-        // Only process JavaScript output files
-        if ((asset as any).type !== "asset" && "code" in (asset as any)) {
-          const code = (asset as any).code;
-          const fileName = (asset as any).fileName || "";
-
-          // Skip if already has the header
-          if (code.includes("@ts-self-types")) {
-            continue;
-          }
-
-          // Determine the corresponding .d.ts file
-          const ext = extname(fileName);
-          const typeDefFile = fileName.replace(ext, ".d.ts");
-
-          // Create the header
-          const header = `/* @ts-self-types="./${typeDefFile}" */\n`;
-
-          // Add header to the code
-          (asset as any).code = header + code;
-        }
-      }
-    },
-  };
-}
